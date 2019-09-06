690f503d69b07f2c14131fac47db4719259d100e
marco987 <pacemarco87@gmail.com>
2019-09-06 14:26:35 +0200

    Creazione file

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Teacher Array</title>
+
+    <?php
+
+    // Utilizzare questo array: https://pastebin.com/CkX3680A
+    // Stampiamo il nostro array mettendo gli insegnanti
+    // in un rettangolo grigio e i PM in un rettangolo verde.
+
+    // url http://localhost/index.php
+
+    ?>
+
+  </head>
+  <body>
+
+    <?php
+
+      
+
+    ?>
+
+
+  </body>
+</html>
