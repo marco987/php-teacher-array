641962de7ab0bfb97af2bebeb6aa3655df03d301
marco987 <pacemarco87@gmail.com>
2019-09-06 14:29:54 +0200

    Inserimento array

--- index.php
+++ index.php
@@ -19,7 +19,32 @@
 
     <?php
 
-      
+      $db = [
+        'teachers' => [
+            [
+                'name' => 'Michele',
+                'lastname' => 'Papagni'
+            ],
+            [
+                'name' => 'Fabio',
+                'lastname' => 'Forghieri'
+            ]
+        ],
+        'pm' => [
+            [
+                'name' => 'Roberto',
+                'lastname' => 'Marazzini'
+            ],
+            [
+                'name' => 'Federico',
+                'lastname' => 'Pellegrini'
+            ]
+        ]
+      ];
+
+    
+
+
 
     ?>
 
